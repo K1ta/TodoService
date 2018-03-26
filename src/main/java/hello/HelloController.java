5bdbd8b0211111670bc479ad3b07ac26f4d79975
nikita <kobalt31@mail.ru>
2018-03-26 16:56:22 +0300

    add connection with database

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,6 +3,11 @@ package hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 @RestController
 public class HelloController {
 
@@ -12,13 +17,34 @@ public class HelloController {
     }
 
     @RequestMapping("/help")
-    public String index2() {
+    public String help() {
         return "Help";
     }
 
-    @RequestMapping("/int")
-    public int index3() {
-        return 20;
+    @RequestMapping("/database")
+    public String database() {
+        String res = "";
+        try {
+            String url = "jdbc:postgresql://localhost:5432/test";
+            String login = "postgres";
+            String password = "postgres";
+            Connection con = DriverManager.getConnection(url, login, password);
+            try {
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM students");
+                while (rs.next()) {
+                    String str = rs.getString("s_id") + ":" + rs.getString(2);
+                    res += "<br>Student: " + str;
+                }
+                rs.close();
+                stmt.close();
+            } finally {
+                con.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 
 }
